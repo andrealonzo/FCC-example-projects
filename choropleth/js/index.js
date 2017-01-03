9eab67e95ce146d966b30d9cde99fbf1f4923149
Andre Alonzo <aalonzo@192.168.1.122>
2017-01-03 09:10:16 -0800

    fixed legend and tooltip

--- choropleth/js/index.js
+++ choropleth/js/index.js
@@ -13,26 +13,26 @@ var tooltip = body.append("div")
 .style("opacity", 0);
 
 // Add title and description
+var heading = body.append("heading");
 
-    var heading = body.append("heading");
-    heading.append("h1")
-      .attr('id', 'title')
-      .text("Monthly Global Land-Surface Temperature");
-    heading.append("h3")
-      .attr('id', 'description')
-      .text("Description")
+heading.append("h1")
+  .attr('id', 'title')
+  .text("United States Median Income");
+
+heading.append("h3")
+  .attr('id', 'description')
+  .text("Data from x")
 
 var unemployment = d3.map();
 
 var path = d3.geoPath();
 
 var x = d3.scaleLinear()
-    .domain([1, 10])
+    .domain([21658, 125635])
     .rangeRound([600, 860]);
 
 var color = d3.scaleThreshold()
     .domain(d3.range(21658, 125635, (125635-21658)/8))
-
     .range(d3.schemeGreens[9]);
 
 var g = svg.append("g")
@@ -64,9 +64,9 @@ g.append("text")
 
 g.call(d3.axisBottom(x)
     .tickSize(13)
-    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickFormat(function(x) { return Math.round(x/1000)})
     .tickValues(color.domain()))
-  .select(".domain")
+    .select(".domain")
     .remove();
 
 d3.queue()
@@ -83,9 +83,20 @@ function ready(error, us, income) {
     .data(topojson.feature(us, us.objects.counties).features)
     .enter().append("path")
       .attr("class", "area")
-      .attr("data-fips", "4")
-      .attr("data-income", "4")
-      .attr("fill", function(d) {
+      .attr("data-fips", function(d) {
+        return d.id
+       })
+      .attr("data-income", function(d) {
+        var result = income.filter(function( obj ) {
+          return obj.fips == d.id;
+        });
+        if(result[0]){
+          return result[0].household_income
+        }
+        //could not find a matching fips id in the data
+        return 0
+       })
+      .attr("fill", function(d) { 
         var result = income.filter(function( obj ) {
           return obj.fips == d.id;
         });
@@ -96,18 +107,33 @@ function ready(error, us, income) {
         return color(0)
        })
       .attr("d", path)
-      .on("mouseover", function(d) {
-        tooltip.style("opacity", .9);
-        tooltip.html(d.rate + '%')
-          .style("left", (d3.event.pageX) + "px")
-          .style("top", (d3.event.pageY - 28) + "px"); })
-          .on("mouseout", function(d) {
-            tooltip.style("opacity", 0);
-          })
-          .append("title")
-          .text(function(d) {
-            return d.rate + "%";
+      .on("mouseover", function(d) {      
+        tooltip.style("opacity", .9); 
+        tooltip.html(function() {
+          var result = income.filter(function( obj ) {
+            return obj.fips == d.id;
+          });
+          if(result[0]){
+            return result[0].household_income
+          }
+          //could not find a matching fips id in the data
+          return 0
+        })
+      .attr("data-income", function() {
+        var result = income.filter(function( obj ) {
+          return obj.fips == d.id;
         });
+        if(result[0]){
+          return result[0].household_income
+        }
+        //could not find a matching fips id in the data
+        return 0
+       })
+          .style("left", (d3.event.pageX) + "px") 
+          .style("top", (d3.event.pageY - 28) + "px"); }) 
+          .on("mouseout", function(d) { 
+            tooltip.style("opacity", 0); 
+          });
 
   svg.append("path")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
