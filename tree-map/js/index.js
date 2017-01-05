d3db29c061c788a82d6364c0a77ab99a8fcb300e
Andre Alonzo <aalonzo@192.168.1.122>
2017-01-05 07:57:05 -0800

    updated treemap with sample video game data

--- tree-map/js/index.js
+++ tree-map/js/index.js
@@ -1,51 +1,64 @@
 const project_name="tree-map"
 
-var width = 960,
-    height = 1060;
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var format = d3.format(",d");
-
-var color = d3.scaleOrdinal()
-    .range(d3.schemeCategory10
-        .map(function(c) { c = d3.rgb(c); c.opacity = 0.6; return c; }));
-
-var stratify = d3.stratify()
-    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+var fader = function(color) { return d3.interpolateRgb(color, "#fff")(0.2); },
+    color = d3.scaleOrdinal(d3.schemeCategory20.map(fader)),
+    format = d3.format(",d");
 
 var treemap = d3.treemap()
+    .tile(d3.treemapResquarify)
     .size([width, height])
-    .padding(1)
-    .round(true);
+    .round(true)
+    .paddingInner(1);
 
-d3.csv("../data/flare.csv", type, function(error, data) {
+d3.json("data/video_game_sales.json", function(error, data) {
   if (error) throw error;
 
-  var root = stratify(data)
-      .sum(function(d) { return d.value; })
+  var root = d3.hierarchy(data)
+      .eachBefore(function(d) {
+        console.log("d",d); 
+        d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name; 
+        console.log("data id", d.data.id);
+      })
+      .sum(sumBySize)
       .sort(function(a, b) { return b.height - a.height || b.value - a.value; });
 
   treemap(root);
 
-  d3.select("body")
-    .selectAll(".node")
+  var cell = svg.selectAll("g")
     .data(root.leaves())
-    .enter().append("div")
-      .attr("class", "node")
-      .attr("title", function(d) { return d.id + "\n" + format(d.value); })
-      .style("left", function(d) { return d.x0 + "px"; })
-      .style("top", function(d) { return d.y0 + "px"; })
-      .style("width", function(d) { return d.x1 - d.x0 + "px"; })
-      .style("height", function(d) { return d.y1 - d.y0 + "px"; })
-      .style("background", function(d) { while (d.depth > 1) d = d.parent; return color(d.id); })
-    .append("div")
-      .attr("class", "node-label")
-      .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g).join("\n"); })
-    .append("div")
-      .attr("class", "node-value")
-      .text(function(d) { return format(d.value); });
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+  cell.append("rect")
+      .attr("id", function(d) { return d.data.id; })
+      .attr("width", function(d) { return d.x1 - d.x0; })
+      .attr("height", function(d) { return d.y1 - d.y0; })
+      .attr("fill", function(d) { return color(d.parent.data.id); });
+
+  cell.append("clipPath")
+      .attr("id", function(d) { return "clip-" + d.data.id; })
+    .append("use")
+      .attr("xlink:href", function(d) { return "#" + d.data.id; });
+
+  cell.append("text")
+      .attr("clip-path", function(d) { return "url(#clip-" + d.data.id + ")"; })
+    .selectAll("tspan")
+      .data(function(d) { return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
+    .enter().append("tspan")
+      .attr("x", 4)
+      .attr("y", function(d, i) { return 13 + i * 10; })
+      .text(function(d) { return d; });
+
+  cell.append("title")
+      .text(function(d) { return d.data.id + "\n" + format(d.value); });
+
 });
 
-function type(d) {
-  d.value = +d.value;
-  return d;
+function sumBySize(d) {
+  return d.size;
 }
+
